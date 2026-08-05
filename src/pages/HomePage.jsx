eb22e97e4073103f457941b6f7fd8b843c6686f0
ImmanA <2312020@nec.edu.in>
2026-08-05 19:39:43 +0530

    Streamline homepage structure by eliminating redundant sections into a clean 9-step corporate narrative

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,8 @@ import { HeroSection } from '../components/Home/HeroSection';
 import { MarqueeSection } from '../components/Home/MarqueeSection';
 import { CircularInfinitySection } from '../components/Home/CircularInfinitySection';
 import { AboutPreviewSection } from '../components/Home/AboutPreviewSection';
-import { RecyclingStorytellingSection } from '../components/Home/RecyclingStorytellingSection';
 import { ProductsShowcaseSection } from '../components/Home/ProductsShowcaseSection';
 import { DataVisualizationSection } from '../components/Home/DataVisualizationSection';
-import { InteractiveEarthSection } from '../components/Home/InteractiveEarthSection';
-import { GlobalNetworkMap } from '../components/Home/GlobalNetworkMap';
 import { TestimonialsSection } from '../components/Home/TestimonialsSection';
 import { PartnersLogos } from '../components/Home/PartnersLogos';
 import { CTASection } from '../components/Home/CTASection';
@@ -15,17 +12,31 @@ import { CTASection } from '../components/Home/CTASection';
 export const HomePage = () => {
   return (
     <div className="home-page-container">
+      {/* 1. Hero Section */}
       <HeroSection />
+
+      {/* 2. Certifications Ticker Marquee */}
       <MarqueeSection />
+
+      {/* 3. Interactive Infinity Loop Circular Economy (Matching Reference Diagram) */}
       <CircularInfinitySection />
+
+      {/* 4. Company Heritage & Vision */}
       <AboutPreviewSection />
-      <RecyclingStorytellingSection />
+
+      {/* 5. Product Portfolio Showcase & Where Our Fabrics Are Used */}
       <ProductsShowcaseSection />
+
+      {/* 6. Manufacturing Data Visualization & Infographics */}
       <DataVisualizationSection />
-      <InteractiveEarthSection />
-      <GlobalNetworkMap />
+
+      {/* 7. Client Testimonials */}
       <TestimonialsSection />
+
+      {/* 8. Global Brand & Industry Certification Partners */}
       <PartnersLogos />
+
+      {/* 9. Full-Width CTA Banner */}
       <CTASection />
     </div>
   );
